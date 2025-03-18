add63d674d4d5ca5d7340d4f6bee75aa1860b91a
404 <sametalpdeger9@gmail.com>
2025-03-18 14:56:08 +0300

    first commit

---
+++ index.ts
@@ -0,0 +1,30 @@
+import { GoogleGenerativeAI, HarmBlockThreshold, HarmCategory } from '@google/generative-ai'
+
+const apiKey = process.env.GEMINI_API_KEY
+if (!apiKey) throw new Error('GEMINI_API_KEY is not set')
+
+const genAI = new GoogleGenerativeAI(apiKey)
+const model = genAI.getGenerativeModel({
+    model: 'gemini-2.0-flash',
+    safetySettings: [
+        {
+            category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+            threshold: HarmBlockThreshold.BLOCK_NONE
+        }
+    ]
+})
+
+while (true) {
+    console.log('\n -------------------- \n')
+    const promptRes = prompt('Enter your prompt:')
+    console.log('\n \n')
+    if (!promptRes) throw new Error('Prompt is empty')
+
+    const result = await model.generateContentStream(promptRes)
+
+    // Log streamed response chunks as they arrive
+    for await (const chunk of result.stream) {
+        const chunkText = chunk.text()
+        console.log(chunkText)
+    }
+}
---
+++ prettier.config.cjs
@@ -0,0 +1,17 @@
+const config = {
+  plugins: [],
+  pluginSearchDirs: false,
+  semi: true,
+  singleQuote: true,
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.js', '*.jsx'],
+      options: {
+        semi: false,
+        singleQuote: true,
+      },
+    },
+  ],
+};
+
+module.exports = config;
